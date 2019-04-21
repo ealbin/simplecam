6f6781855cecb80a2f9f8a61d07371f8b9c461d5
Eric Albin <Eric.K.Albin@gmail.com>
2019-04-20 23:30:34 -0700

    whatever

--- app/src/main/java/sux/tests/capturestartstop/MainActivity.java
+++ app/src/main/java/sux/tests/capturestartstop/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         Log.e("F", "0");
         setContentView(R.layout.activity_main);
 
